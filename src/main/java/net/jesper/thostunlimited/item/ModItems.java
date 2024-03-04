03d53e901b26bb02c67b0cdcdea3a443078101fa
Jesplay <jesper@diesteuers.de>
2024-03-04 22:39:59 +0100

    first update?

---
+++ src/main/java/net/jesper/thostunlimited/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.jesper.thostunlimited.item;
+
+import net.jesper.thostunlimited.ThorMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ThorMod.MODID);
+
+    public static final RegistryObject<Item> GABEL = ITEMS.register("gabel",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
